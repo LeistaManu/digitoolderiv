61842ed1ca88ca8a5d00151baa29b01d55edaf84
Rambo Leista <148265826+LeistaManu@users.noreply.github.com>
2026-08-02 19:18:43 +0300

    Update OAuth client ID in app.tsx

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -52,7 +52,7 @@ async function login() {
 
     const params = new URLSearchParams({
       response_type: "code",
-      client_id: "33Vxdb9Yf1exXgyW3vms1", // OAuth Client ID
+      client_id: "340fKqgQxBtyfOpYwkRmA",
       redirect_uri: "https://digittoolderiv.site/auth/callback",
       scope: "trade account_manage application_read payment",
       state,
