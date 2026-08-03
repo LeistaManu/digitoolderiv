383849c2e967b1f9f08ea35852738cf8119a0ced
Rambo Leista <148265826+LeistaManu@users.noreply.github.com>
2026-08-03 18:35:43 +0300

    Update redirect URI for authentication callback

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -55,7 +55,7 @@ async function login() {
     const params = new URLSearchParams({
       response_type: 'code',
       client_id: '340fKqgQxBtyfOpYwkRmA',
-      redirect_uri: 'https://www.digittoolderiv.site/auth/callback',
+      redirect_uri: 'https://digitoolderiv.vercel.app/auth/callback',
       state,
       code_challenge: challenge,
       code_challenge_method: 'S256',
